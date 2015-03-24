94e3e8dfde2aefd82c52f452fb1a094971e25940
abdullahnabeel <abdullah_nabeel@Hotmail.com>
2015-03-24 23:20:53 +0500

    Part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,25 +2,59 @@
 class MoviesController < ApplicationController
 
   def index
+    session[:ratings] = [ 'G', 'R', 'PG-13' , 'PG', 'NC-17'];
     sby = params[:sort_by]
     @slite = "no"
     @drel = "no"
     @all_ratings = ['G','PG','PG-13','R','NC-17']
     disrat = params[:ratings]
+
+    if session[:sort_by]!=sby
+      session[:sort_by] = sby
+    end
+    if session[:ratings]!=disrat
+      session[:ratings]=disrat
+    end
+    if sby == nil && disrat == nil && session[:sort_by] != nil
+      sby = session[:sort_by]
+    end
+     if sby == nil && disrat != nil && session[:ratings] != nil
+      disrat = session[:ratings]
+    end
+
     if sby == "title" && disrat == nil
-      @movies = Movie.find(:all,:order => "title")
-      @slite = "hilite"
-    elsif sby == "release_date" && disrat == nil
+      if session[:ratings] != nil
+      @movies = Movie.find(:all,:conditions => {:rating => session[:ratings].keys},:order => "title")
+      else 
       @movies = Movie.find(:all,:order => "release_date")
+      end
+      @slite = "hilite"
+      session[:sort_by] = "title"
+    elsif sby == "release_date" && disrat == nil
+      if session[:ratings] != nil
+      @movies = Movie.find(:all,:conditions => {:rating => session[:ratings].keys},:order => "release_date")
+      else 
+      @movies = Movie.find(:all,:order => "release_date")
+      end
+      session[:sort_by] = "release_date"
       @drel = "hilite"
     elsif sby == nil && disrat != nil
+      if session[:sort_by] != nil
+      @movies = Movie.find(:all, :conditions => {:rating => disrat.keys},:order => session[:sort_by])
+      else 
       @movies = Movie.find(:all, :conditions => {:rating => disrat.keys})
+      end
+      session[:ratings] = disrat
     elsif sby == "title" && disrat != nil
       @movies = Movie.find(:all, :conditions => {:rating => disrat.keys},:order => "title")
       @slite = "hilite"
+      session[:sort_by] = "title"
+      session[:ratings] = disrat
     elsif sby == "release_date" && disrat != nil
       @movies = Movie.find(:all, :conditions => {:rating => disrat.keys},:order => "release_date")
       @drel = "hilite"
+      session[:sort_by] = "release_date"
+      session[:ratings] = disrat
     else 
       @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :description, :release_date, :all_ratings
+  attr_accessible :title, :rating, :description, :release_date
+  @@all_ratings = ['G', 'PG', 'PG-13', 'R', 'NC-17']
+  def get_ratings
+    all_ratings
+  end
 end
